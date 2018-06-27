9749c750e5e42d0552b34f7d44bd840c6ee3c44e
maph maphskiy <maphskiy@gmail.com>
2018-06-27 15:46:24 +0300

    изменил видимость переменной

--- src/Services/BaseWithOperation/BaseWithOperationService.ts
+++ src/Services/BaseWithOperation/BaseWithOperationService.ts
@@ -1,7 +1,7 @@
 import { IOperationService } from "../../Types/OperationService/IOperationService";
 
 export class BaseWithOperationService {
-    private _operation: IOperationService;
+    _operation: IOperationService;
     constructor(operationService: IOperationService) {
         this._operation = operationService;
     }
